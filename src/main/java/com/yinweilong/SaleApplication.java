6710ee7a4ce9a68c44eef223ea35f97f96b25681
yinweilong <290517897@qq.com>
2015-11-14 13:53:17 +0800

    create

---
+++ src/main/java/com/yinweilong/SaleApplication.java
@@ -0,0 +1,12 @@
+package com.yinweilong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaleApplication.class, args);
+    }
+}
